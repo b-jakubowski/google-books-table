c4a5a9bbf3a1bb262d551e7372acd7de7ab28c20
Bartek Jakubowski <bjakubov@gmail.com>
2024-06-20 20:29:44 +0200

    Add basic search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, TextField } from '@mui/material'
+import { FC, useState } from 'react'
+
+export type SearchProps = {
+  onSearchClick: (value: string) => void
+}
+
+export const Search: FC<SearchProps> = ({ onSearchClick }) => {
+  const [searchValue, setSearchValue] = useState('')
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <TextField
+        label="Search for books"
+        variant="outlined"
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
+      <Button
+        sx={{ marginLeft: 2 }}
+        variant="outlined"
+        onClick={() => onSearchClick(searchValue)}
+      >
+        Search
+      </Button>
+    </Box>
+  )
+}
